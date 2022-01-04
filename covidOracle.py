ae0e24658b585472fcda7c408fc0832d26e00c57
Machalka <al1999dk@gmail.com>
2022-01-05 02:41:42 +0300

    создал CovidOracle

---
+++ covidOracle.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import norm
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import itertools
+import warnings
+warnings.filterwarnings("ignore")
+
+
+class CovidOracle():
+    def __init__():
+        pass
